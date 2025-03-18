8c9bdef9f968d6ff9204fccfe052179eeb6cec79
Hugo Palma <hugo.m.palma@gmail.com>
2025-03-18 18:30:20 +0000

    fix tests

--- src/test/kotlin/org/hugopalma/domaintranslator/dictionary/DictionaryTest.kt
+++ src/test/kotlin/org/hugopalma/domaintranslator/dictionary/DictionaryTest.kt
@@ -9,21 +9,21 @@ class DictionaryTest {
 
     @Test
     fun `exact word is translated`() {
-        val dictionary = Dictionary(mapOf(Pair("somekey", "translation")))
+        val dictionary = Dictionary(mapOf(Pair("somekey", "translation")), 0)
 
         assertEquals("Translation", dictionary.translate("SomeKey"))
     }
 
     @Test
     fun `a word in the middle of the text is translated`() {
-        val dictionary = Dictionary(mapOf(Pair("somekey", "translation")))
+        val dictionary = Dictionary(mapOf(Pair("somekey", "translation")), 0)
 
         assertEquals("settranslationafter", dictionary.translate("setSomeKeyAfter")?.toLowerCasePreservingASCIIRules())
     }
 
     @Test
     fun `camelcase is preserved`() {
-        val dictionary = Dictionary(mapOf(Pair("somekey", "my translation")))
+        val dictionary = Dictionary(mapOf(Pair("somekey", "my translation")), 0)
 
         assertEquals("setMyTranslationAfter", dictionary.translate("setSomeKeyAfter"))
         assertEquals("setmyTranslationAfter", dictionary.translate("setsomeKeyAfter"))
@@ -32,21 +32,21 @@ class DictionaryTest {
 
     @Test
     fun `snakecase is preserved`() {
-        val dictionary = Dictionary(mapOf(Pair("somekey", "my translation")))
+        val dictionary = Dictionary(mapOf(Pair("somekey", "my translation")), 0)
 
         assertEquals("FIRST_MY_TRANSLATION_AFTER", dictionary.translate("FIRST_SOMEKEY_AFTER"))
     }
 
     @Test
     fun `if translation is same as original`() {
-        val dictionary = Dictionary(mapOf(Pair("word", "word")))
+        val dictionary = Dictionary(mapOf(Pair("word", "word")), 0)
 
         assertNull(dictionary.translate("word"))
     }
 
     @Test
     fun `german umlauts`() {
-        val dictionary = Dictionary(mapOf(Pair("stückzahl", "quantity"), Pair("straße", "road")))
+        val dictionary = Dictionary(mapOf(Pair("stückzahl", "quantity"), Pair("straße", "road")), 0)
 
         assertEquals("quantity", dictionary.translate("stueckzahl"))
         assertEquals("quantity", dictionary.translate("stückzahl"))
